3946fab96bcee27d36492c86bd2294c57680dde9
DeveloperFire <alessandro@mizzaro.ml>
2016-06-25 18:45:12 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,12 +1,10 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-
 if(!$update)
 {
   exit;
 }
-
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -15,19 +13,17 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
-
 $text = trim($text);
 $text = strtolower($text);
-
 header("Content-Type: application/json");
 $response = '';
 if(strpos($text, "/start") === 0 || $text=="ciao")
 {
 	$response = "Ciao $firstname, benvenuto!";
 }
-elseif($text=="sito")
+elseif($text=="domanda 1")
 {
-	$response = "developerfire.ml";
+	$response = "risposta 1";
 }
 elseif($text=="domanda 2")
 {
@@ -39,4 +35,5 @@ else
 }
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
-echo json_encode($parameters); 
+echo json_encode($parameters);
+?>
